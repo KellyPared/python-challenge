ed150fa61f52f98210155dc777f2ec32a4ee394f
Kelly Schuster-Paredes <40581033+KellyPared@users.noreply.github.com>
2022-12-13 18:29:04 -0500

    exra functions added

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,19 +3,52 @@
 
 import pandas as pd
 
+def header():
+    '''Gives the app a header.'''
+    
+    print("Financial Analysis")
+    print("------------------------")
+    
+    
 def importing_csv():
+    '''imports the csv using pandas saves as df'''
+    
     df = pd.read_csv(r'/Users/kellypared/Documents/GitHub/python-challenge/PyBank/Resources/budget_data.csv')
     print(df)
+    return df
 
-importing_csv()
 
-def total_months():
-    # Calculates the total number of months
+def total_months(df):
+    ''' Calculates the total number of months using pandas count'''
     
-    # find the column
-    pass
+    count_months = df['Date'].count()
+    print(f"Total Months: {count_months}")
+    return count_months
+    
+
+def profits_losses(df):
+    '''Sums up the total amount of profits and losses'''
+
+    sum_p_l = df['Profit/Losses'].sum()
+    print(f'Total: ${sum_p_l}')
+    return sum_p_l
 
 
+def average_change(df):
+    '''Calculate the average change.'''
+
+    avg_change = df['Profit/Losses'].std()
+    print(avg_change)
+
+
+def main():
+    header()
+    data = importing_csv()
+    count = total_months(data)
+    sum_p_l = profits_losses(data)
+    average_change(data)
+
+main()
 # Calculate the net amounth of Profits/Losses over the entire period
 
 # Calculate the changes in Profit/Losses over the entire period and average the changes.
