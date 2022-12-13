d19c8e90f9970389301e5b008fb6c8a6cd74d98d
Kelly Schuster-Paredes <40581033+KellyPared@users.noreply.github.com>
2022-12-13 11:09:09 -0500

    main comments added

---
+++ PyBank/main.py
@@ -0,0 +1,19 @@
+'''This script analyzes financial records with the Profit/Losses.'''
+
+def total_months():
+    # Calculates the total number of months
+    
+    # find the column
+    pass
+
+
+# Calculate the net amounth of Profits/Losses over the entire period
+
+# Calculate the changes in Profit/Losses over the entire period and average the changes.
+
+# The greatest increase in profit/losses(date and amount) over entire period
+
+# The greatest decrease in profits(date and amount) over the entire period.
+
+# print analysis to terminal
+# export file as a text
